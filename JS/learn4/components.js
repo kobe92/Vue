ef3d04cc84e47f4b8026d33bc4ec8ca80d14e46d
Xiaowen's Dog <751204363@qq.com>
2019-03-22 15:01:49 +0800

    add vue.js commponents part learn

--- JS/learn4/components.js
+++ JS/learn4/components.js
@@ -22,3 +22,37 @@ new Vue({
 		]
 	}
 })
+
+Vue.component('blog',{
+	props:['post'],
+	template:'<div class="blog"><h3>{{post.title}}</h3><div v-html="post.content"></div></div>'
+})
+
+new Vue({
+	el: '#blog-events-demo',
+	data:{
+		posts:[
+			{id:1, title:'this is a title', content:"vyote vya mungu wetu na mf"},
+			{id:2, title:'Blogging with Vue', content:'Blogging with Vue content'},
+			{id:3, title:'My Vue is so fun', content:'My Vue is so funt'}
+		],
+		postFontSize:1
+	}
+})
+
+Vue.component('blog-p',{
+	props:['post'],
+	template:'<div class="blog-p"><h3>{{post.title}}</h3><button>Enlarge text</button><div v-html="post.content"></div></div>'
+})
+
+new Vue({
+	el:'#blog-p',
+	data:{
+		posts:[
+			{id:1, title:'My journey with Vue', content:'My journey with Vue content'},
+			{id:2, title:'Blogging with Vue', content:'Blogging with Vue content'},
+			{id:3, title:'My Vue is so fun', content:'My Vue is so funt'}
+		],
+		postFontSize:1
+	}
+})
